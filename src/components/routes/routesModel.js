8ef4d36ba5330aaf8f4a86ccdb8c2a211b826a48
rileyfullstack <yaniv31313@gmail.com>
2024-02-13 05:40:52 +0200

    Updated routesModel to reflect on future pages.

--- src/components/routes/routesModel.js
+++ src/components/routes/routesModel.js
@@ -1,6 +1,17 @@
 const ROUTES = {
   HOME: "/",
-
+  ABOUT: "/about",
+  LOGIN: "/login",
+  REGISTER: "/register",
+  ORDERS: "/orders",
+  ORDER: "/order",
+  USERMANAGEMENT: "/usermanagement",
+  USER: "/user",
+  NOTFOUND: "/404",
+  CART: "/cart",
+  CHECKOUT: "/checkout",
+  BIKEPOST: "/bikepost",
+  SELLPOSTS: "/sellposts",
 };
 
 export default ROUTES;
