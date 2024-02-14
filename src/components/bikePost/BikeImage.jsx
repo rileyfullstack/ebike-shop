5017c7ff58b70ae89391a42835bbe123d2ae7970
rileyfullstack <yaniv31313@gmail.com>
2024-02-14 21:44:51 +0200

    Added a better layout to the BikePost.

--- src/components/bikePost/BikeImage.jsx
+++ src/components/bikePost/BikeImage.jsx
@@ -1,15 +1,19 @@
-import { CardMedia, Box } from '@mui/material'
+import { CardMedia, Box, CardActionArea } from '@mui/material'
 import React from 'react'
 
-export default function BikeImage({Image}) {
-  return (
-      <Box sx={{ width: '25%', height: '100%' }}> {/* Container to control width and height */}
-        <CardMedia
-            component="img"
-            image={Image.url}
-            alt={Image.alt}
-            sx={{ width: '100%', height: '100%' }} // Make image fully fill the container
-        />
-      </Box>
-  );
+export default function BikeImage({ Image }) {
+    return (
+
+        //For some reason all of them need 100% height to fill the container. Don't ask me why. Just keep it that way.
+        <Box sx={{ width: '240px', height: '240px' }}> {/* Container to control width and height */}
+            <CardActionArea sx={{ height: '100%' }}>
+                <CardMedia
+                    component="img"
+                    image={Image.url}
+                    alt={Image.alt}
+                    sx={{ width: '100%', height: '100%'}} // Make image fully fill the container
+                />
+            </CardActionArea>
+        </Box>
+    );
 }
--- src/components/bikePost/BikePost.jsx
+++ src/components/bikePost/BikePost.jsx
@@ -1,14 +1,21 @@
-import { Card } from '@mui/material'
+import { Card, CardActionArea } from '@mui/material'
 import React from 'react'
 import BikeImage from './BikeImage';
 
 export default function BikePost({ bikePost }) {
 
-    //const image = { url: bikePost?.image?.url, alt: bikePost?.image?.alt };
+    //example image until I can import it from the bike post.
     const image = { url: 'https://media.wired.com/photos/64c1a027a6c1fece8f4bb578/191:100/w_2580,c_limit/Aventon-Abound-Gear.jpg', alt: 'bike' };
 
     return (
-        <Card sx={{ backgroundColor: 'lightGray', width: '100%', height: '200px', flexDirection: 'column' }}>
+        <Card sx={{
+            backgroundColor: '#f5f5f5',
+            width: '100%',
+            height: '270px',
+            flexDirection: 'column',
+            border: '2px solid black',
+            borderRadius: '0'
+        }}>
             <BikeImage Image={image} />
         </Card>
     )
