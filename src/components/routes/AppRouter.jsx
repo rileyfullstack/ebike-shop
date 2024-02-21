99eb2490b064c76d87a58b689a605ac03410647a
rileyfullstack <yaniv31313@gmail.com>
2024-02-21 14:15:52 +0200

    Add new page and add it to router

--- src/components/routes/AppRouter.jsx
+++ src/components/routes/AppRouter.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ROUTES from './routesModel'
 import BikePostPage from '../bikePost/BikePostPage';
+import BikePostPurchasePage from '../bikePost/BikePostPurchasePage';
 
 const AppRouter = () => {
     return (
             <Routes>
                 <Route path={ROUTES.HOME} element={<BikePostPage/>} />
+                <Route path={`${ROUTES.BIKEPOST}/:id`} element={<BikePostPurchasePage/>} />
             </Routes>
     );
 };
